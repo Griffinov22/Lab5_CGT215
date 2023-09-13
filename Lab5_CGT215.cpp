2fb2f7bac0d18784cb58701176cada2f9a7923fa
Griffin Overmyer <griffinovermyer@gmail.com>
2023-09-13 11:29:23 -0400

    implemented cypher

--- Lab5_CGT215.cpp
+++ Lab5_CGT215.cpp
@@ -2,19 +2,53 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void cypherMessage(string& message) {
+    vector<char> cypher = { 'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' };
+
+    //loop message
+    for (int i = 0; i < message.length(); i++) {
+        //get ascii number of current char
+        int asciiVal(message[i]);
+
+        if (asciiVal >= 65 && asciiVal <= 90) {
+            //is a capital letter
+            asciiVal -= 65;
+            message[i] = cypher[asciiVal];
+        }
+        else if (asciiVal >= 97 && asciiVal <= 122) {
+            //lowercase letter
+            asciiVal -= 97;
+            message[i] = tolower(cypher[asciiVal]);
+        }
+    }
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int running(1);
+    string ShouldEnd;
+    string message;
+
+    while (running != 0) {
+        cout << "Enter your message to be cyphered: ";
+        getline(cin, message);
+        //cypher text function & pass by value
+        cypherMessage(message);
+        cout << "Your cyphered message: " << message << endl;
+        cout << "Would you like to continue (y/n)?: ";
+        cin >> ShouldEnd;
+
+        //checking if should end
+        ShouldEnd == "n" ? running = 0 : running = 1;
+        if (running) {
+            message.clear();
+            //clearing cin buffer
+            cin.ignore();
+        }
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
